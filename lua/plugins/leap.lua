9b15c41d73343aa65b09ce8bce3f27491b71ece3
Diogo Ferreira <192282837+diogof146@users.noreply.github.com>
2025-02-15 01:06:40 +0000

    Removed forward and backwards search

--- lua/plugins/leap.lua
+++ lua/plugins/leap.lua
@@ -9,7 +9,11 @@ return {
 		vim.keymap.set({ "n", "x" }, "f", "<Plug>(leap)")
 		vim.keymap.set("n", "F", "<Plug>(leap-from-window)")
 
-		vim.keymap.set("o", "<leader>s", "<Plug>(leap-forward)")
-		vim.keymap.set("o", "<leader>S", "<Plug>(leap-backward)")
+    -- The following are commented because I'm not currently using them but
+    -- they'd be useful for a bigger change at instant leaps
+
+		-- vim.keymap.set("o", "<leader>s", "<Plug>(leap-forward)")
+		-- vim.keymap.set("o", "<leader>S", "<Plug>(leap-backward)")
+
 	end,
 }
