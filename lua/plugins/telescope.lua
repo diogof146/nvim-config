b7672ed8f4c325ff3db663cd836bd077dbaf6d0d
Diogo Ferreira <192282837+diogof146@users.noreply.github.com>
2026-02-06 04:07:51 +0000

    telescope improvements

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,62 +1,124 @@
 -- Fuzzy Finder and Search for Neovim
 
 return {
-	"nvim-telescope/telescope.nvim", -- Plugin repository
-	branch = "0.1.x", -- Specify to use the stable branch for reliability and compatibility
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim", -- Required for async operations and utility functions
-		"nvim-telescope/telescope-ui-select.nvim", -- Provides an enhanced UI for selections (dropdown theme)
+		"nvim-telescope/telescope-ui-select.nvim", -- Enhanced UI for selections
 	},
 	config = function()
-		-- Import necessary Telescope functions
-		local telescope = require("telescope") -- Telescope core
-		local builtin = require("telescope.builtin") -- Default built-in Telescope pickers
+		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
+
+		-- Smart root detection: git root → parent dir → cwd
+		local function get_smart_root()
+			-- Try to find git root first
+			local git_root = vim.fn.systemlist("git rev-parse --show-toplevel 2>/dev/null")[1]
+			if vim.v.shell_error == 0 and git_root and git_root ~= "" then
+				return git_root
+			end
+
+			-- No git? Use parent directory of current file
+			local current_file = vim.fn.expand("%:p")
+			if current_file ~= "" then
+				return vim.fn.fnamemodify(current_file, ":h:h") -- parent dir
+			end
+
+			-- Fallback to current working directory
+			return vim.fn.getcwd()
+		end
 
 		-- Global Telescope configuration
 		telescope.setup({
 			defaults = {
-				path_display = { truncate = 20 },
+				-- Better path display
+				path_display = { "smart" },
+
+				-- Faster search with ripgrep
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden", -- Search hidden files
+					"--glob=!.git/", -- But ignore .git directory
+				},
+
 				-- File ignore patterns
 				file_ignore_patterns = {
-					"node_modules", -- Exclude node_modules folder from search results
-					".git", -- Exclude .git directory (version control files)
-					".cache", -- Exclude cache files for faster searching
+					"node_modules",
+					".git/",
+					".cache",
+					"%.class", -- Java compiled files
+					"%.o", -- C object files
+					"%.pyc", -- Python bytecode
+				},
+
+				-- Layout
+				layout_config = {
+					horizontal = {
+						preview_width = 0.55,
+					},
 				},
 			},
 
-			-- Extensions configuration (in this case, the ui-select extension)
+			-- Extensions configuration
 			extensions = {
 				["ui-select"] = {
-					-- Load the dropdown theme for the UI select extension
 					require("telescope.themes").get_dropdown({}),
 				},
 			},
 		})
 
-		-- Enable the ui-select extension which provides a nicer UI for selections
+		-- Load extensions
 		telescope.load_extension("ui-select")
 
 		-- Keybinding Configuration
-		-- Map <leader>ff to find files in the parent directory
+
+		-- <leader>ff - Find files (smart root: git → parent → cwd)
 		vim.keymap.set("n", "<leader>ff", function()
-			-- Gets the parent directory of the current file for a broader search scope
-			local parent_dir = vim.fn.fnamemodify(vim.fn.expand("%:p:h"), ":h")
-			-- Use Telescope's find_files picker to search files within the parent directory
-			builtin.find_files({ cwd = parent_dir })
-		end, { noremap = true }) -- Non-recursive mapping, so the key won't trigger other mappings
+			builtin.find_files({ cwd = get_smart_root() })
+		end, { noremap = true, desc = "Find files" })
 
-		-- Map <leader>fg to live grep in the parent directory
+		-- <leader>fg - Live grep (smart root: git → parent → cwd)
 		vim.keymap.set("n", "<leader>fg", function()
-			-- Gets the parent directory of the current file for a broader search scope
-			local parent_dir = vim.fn.fnamemodify(vim.fn.expand("%:p:h"), ":h")
-			-- Use Telescope's live_grep picker to search for text within files in the parent directory
-			builtin.live_grep({ cwd = parent_dir })
-		end, { noremap = true }) -- Non-recursive mapping
+			builtin.live_grep({ cwd = get_smart_root() })
+		end, { noremap = true, desc = "Live grep" })
 
-		-- Map <leader>fb to open buffer picker (displays a list of open buffers)
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, { noremap = true })
+		-- <leader>fb - Open buffers
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { noremap = true, desc = "Find buffers" })
 
-		-- Map <leader>fh to open help tags picker (displays a list of available help files)
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { noremap = true })
+		-- <leader>fh - Help tags
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { noremap = true, desc = "Help tags" })
+
+		-- <leader>fr - Recently opened files
+		vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { noremap = true, desc = "Recent files" })
+
+		-- <leader>fw - Search word under cursor (in project)
+		vim.keymap.set("n", "<leader>fw", function()
+			builtin.grep_string({ cwd = get_smart_root() })
+		end, { noremap = true, desc = "Find word under cursor" })
+
+		-- <leader>fc - Search in current buffer (fuzzy find current file)
+		vim.keymap.set("n", "<leader>fc", builtin.current_buffer_fuzzy_find, {
+			noremap = true,
+			desc = "Find in current buffer",
+		})
+
+		-- <leader>fd - Search diagnostics (LSP errors/warnings)
+		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { noremap = true, desc = "Find diagnostics" })
+
+		-- <leader>fs - Search symbols in current file (functions, classes, etc.)
+		vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, {
+			noremap = true,
+			desc = "Find symbols in file",
+		})
+
+		-- <leader>fk - Keymaps (see all your keybindings)
+		vim.keymap.set("n", "<leader>fk", builtin.keymaps, { noremap = true, desc = "Find keymaps" })
 	end,
 }
